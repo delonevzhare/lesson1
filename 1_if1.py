ced0be501f620dea89daeee0986a97436a16c850
George Nidekker <georgenidekker@gmail.com>
2024-05-25 14:07:50 +0300

    Правки в файле 1_if1.py

--- 1_if1.py
+++ 1_if1.py
@@ -1,15 +1,15 @@
 age = int(input("Введите плиз свой возраст:"))
 
-def your_duty():
-    if age <= 4:
-        duty = "Детский сад"
-    elif age > 4 and age <= 18:
+def your_duty(age):
+    if 4 < age <= 18:
         duty = "Школа"
-    elif age > 18 and age <= 23:
+    elif 18 < age <= 23:
         duty = "ВУЗ"
+    elif age <= 4:
+        duty = "Детский сад"
     else:
         duty = "Работа"
     
     print(duty)
 
-your_duty()
+your_duty(age)
